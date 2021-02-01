57db29f1d46ae7f45167f66d3e0f560cdb7dd138
lh <im.lh@hotmail.com>
2021-02-01 16:25:22 +0800

    add echarts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="myChart" :style="{ width: '300px', height: '300px' }"></div>
   </div>
 </template>
 
 <script lang="ts">
+import * as echarts from 'echarts';
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  mounted() {
+      const myChartEle = document.getElementById("myChart");
+      if(myChartEle){
+        let myChart = echarts.init(myChartEle);
+        // 绘制图表
+        myChart.setOption({
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            xAxis: {
+                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
+      }
   },
 });
 </script>
