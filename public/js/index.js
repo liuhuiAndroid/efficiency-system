3c0ac54f2a534a5cd1ae080142652328f1d12882
“sa” <550338623@qq.com>
2021-02-03 17:50:59 +0800

    no message

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+ 	new Vue({
+ 	  el: '#nav',
+ 	  data: {
+ 	  	web_name: "光魄能效云系统",
+ 	  	nav: ["首页","标杆组串","实时检测","设备预警","能效分析","运维建议","系统设置"],
+ 	  	date: "2020-12-21",
+ 	  	exit: "退出系统"
+ 	  },
+ 	  methods: {
+ 		aaa: function(){},
+ 		bbb: function(){}		
+ 	  }
+ 	})
