8ea7652ff19f904d6050a408c680ee5891f7866f
LiuHuiAndroid <642404044@qq.com>
2021-02-07 21:10:00 +0800

    axios.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// test axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://api.vikingship.xyz/api'
+// Add a request interceptor
+axios.interceptors.request.use(config => {
+  config.params = { ...config.params, icode: 'aaa' }
+  return config
+})
+axios.get('/columns', { params: { key: 'hello' } }).then(resp => {
+  console.log(resp.data)
+})
+
 createApp(App).use(router).mount('#app')
