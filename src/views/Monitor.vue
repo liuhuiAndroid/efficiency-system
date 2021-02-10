7119c9050445fee447cae672431c57b1adc05847
lh <im.lh@hotmail.com>
2021-02-10 15:44:36 +0800

    test

--- src/views/Monitor.vue
+++ src/views/Monitor.vue
@@ -2,12 +2,12 @@
   <div class="container">
     <div>升压变</div>
     <el-row :gutter="10">
-      <el-col :span="5" v-for="column in testData" :key="column.id">
+      <el-col :span="5" v-for="inverter in inverterList" :key="inverter.deviceId">
         <el-card>
           <div class="grid-content">
-            <h5>{{column.title}}</h5>
-            <p>{{column.description}}</p>
-            <el-button plain>进入专栏</el-button>
+            <h5>{{inverter.deviceName}}</h5>
+            <p>{{inverter.deviceId}}</p>
+            <el-button plain>{{inverter.subDevices}}</el-button>
           </div>
         </el-card>
       </el-col>
@@ -26,24 +26,24 @@
     </el-row>
     <div>汇流箱</div>
     <el-row :gutter="10">
-      <el-col :span="5" v-for="column in testData" :key="column.id">
+      <el-col :span="5" v-for="inverter in inverterList" :key="inverter.deviceId">
         <el-card>
           <div class="grid-content">
-            <h5>{{column.title}}</h5>
-            <p>{{column.description}}</p>
-            <el-button plain>进入专栏</el-button>
+            <h5>{{inverter.deviceName}}</h5>
+            <p>{{inverter.deviceId}}</p>
+            <el-button plain>{{inverter.subDevices}}</el-button>
           </div>
         </el-card>
       </el-col>
     </el-row>
     <div>光伏组串</div>
     <el-row :gutter="10">
-      <el-col :span="5" v-for="column in testData" :key="column.id">
+      <el-col :span="5" v-for="inverter in inverterList" :key="inverter.deviceId">
         <el-card>
           <div class="grid-content">
-            <h5>{{column.title}}</h5>
-            <p>{{column.description}}</p>
-            <el-button plain>进入专栏</el-button>
+            <h5>{{inverter.deviceName}}</h5>
+            <p>{{inverter.deviceId}}</p>
+            <el-button plain>{{inverter.subDevices}}</el-button>
           </div>
         </el-card>
       </el-col>
@@ -81,50 +81,6 @@ export default defineComponent({
         })
       }
     }
-    const testData: ColumnProps[] = [
-      {
-        id: 1,
-        title: 'test1的专栏',
-        description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      },
-      {
-        id: 2,
-        title: 'test2的专栏',
-        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      },
-      {
-        id: 3,
-        title: 'test3的专栏',
-        description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      },
-      {
-        id: 4,
-        title: 'test3的专栏',
-        description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      },
-      {
-        id: 5,
-        title: 'test3的专栏',
-        description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      },
-      {
-        id: 6,
-        title: 'test3的专栏',
-        description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      },
-      {
-        id: 7,
-        title: 'test3的专栏',
-        description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-      }
-    ]
     // 逆变器
     const inverterList: InverterProps[] = [
       {
@@ -144,7 +100,6 @@ export default defineComponent({
     ]
     return {
       logout,
-      testData,
       inverterList
     }
   }
